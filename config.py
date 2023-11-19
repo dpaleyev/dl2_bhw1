303df667c01eb1673d4cb00820ebae518190259b
Daniel Paleyev <danny.paleyev@gmail.com>
2023-11-19 08:32:21 +0300

    Dataset for TinyStories

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 JSON_DIR = "/Users/danny.paleyev/dl2_bhw1/TinyStories_all_data"
 TXT_DIR = "/Users/danny.paleyev/dl2_bhw1/TinyStories_txt"
+DATASET_DIR = "/Users/danny.paleyev/dl2_bhw1/tokenized"
 
 TOKENIZER_PATH = "/Users/danny.paleyev/dl2_bhw1/tokenizer.model"
 VOCAB_SIZE = 32000
--- data.py
+++ data.py
@@ -3,6 +3,8 @@ from tqdm import tqdm
 import os
 import json
 import glob
+import torch
+import numpy as np
 
 import config
 
@@ -33,7 +35,45 @@ def get_tokenizer():
             eos_id=3,
         )
     return spm.SentencePieceProcessor(model_file=config.TOKENIZER_PATH)
+
+
+class TinyStoriesDataset(torch.utils.data.Dataset):
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+        self.data_files = []
+        self.load_data()
+
+    def load_data(self):
+        for filename in tqdm(os.listdir(config.JSON_DIR)[:2], "Loading data"):
+            if not filename.endswith('.json'):
+                continue
+            with open(os.path.join(config.JSON_DIR, filename), 'r') as file_json:
+                data = json.load(file_json)
+                for idx, item in enumerate(data):
+                    tokens = self.tokenizer.encode(item['story'])
+                    npy_filename = f"{filename}_{idx}.npy"
+                    np.save(os.path.join(config.DATASET_DIR, npy_filename), tokens)
+                    self.data_files.append(npy_filename)
+
+    def __len__(self):
+        return len(self.data_files)
+
+    def __getitem__(self, idx):
+        tokens = np.load(os.path.join(config.DATASET_DIR, self.data_files[idx]))
+        return tokens
+
+    def get_collator(self):
+        BOS_ID = self.tokenizer.bos_id()
+        EOS_ID = self.tokenizer.eos_id()
+        PAD_ID = self.tokenizer.pad_id()
+
+        def collate_fn(batch):
+            padded_batch = [torch.tensor([BOS_ID] + item + [EOS_ID]) for item in batch]
+            padded_batch = torch.nn.utils.rnn.pad_sequence(padded_batch, padding_value=PAD_ID, batch_first=True)
+            padding_mask = (padded_batch == PAD_ID)
+            return {
+                "tokens": padded_batch,
+                "padding_mask": padding_mask,
+            }
         
-
-
-
+        return collate_fn
