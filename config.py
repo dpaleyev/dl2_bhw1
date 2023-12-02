cfb7ec37f02be74edd7b18cf7e85fdd93b46a6b2
Daniel Paleyev <danny.paleyev@gmail.com>
2023-12-03 01:49:57 +0300

    config

--- config.py
+++ config.py
@@ -4,5 +4,16 @@ DATASET_DIR = "/Users/danny.paleyev/dl2_bhw1/tokenized"
 VAL_TXT = "/Users/danny.paleyev/dl2_bhw1/TinyStoriesV3-GPT4-valid.txt"
 
 TOKENIZER_PATH = "/Users/danny.paleyev/dl2_bhw1/tokenizer.model"
-VOCAB_SIZE = 32000
+VOCAB_SIZE = 16000
 MAX_SEQ_LEN = 256
+
+LEARNING_RATE = 3e-4
+WEIGHT_DECAY = 1e-5
+
+BATCH_SIZE = 64
+EPOCHS = 25
+
+EMBED_DIM = 512
+N_HEADS = 4
+HIDDEN_DIM = 512
+NUM_LAYERS = 2
